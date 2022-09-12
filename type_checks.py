eefdb5a3b22bfb79fadb643aff5b5fc8a0786d90
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-09-12 12:50:29 +0530

    create-run-1

--- type_checks.py
+++ type_checks.py
@@ -3,6 +3,9 @@ import os
 import sys
 from collection import defaultdict
 
+def say_hello():
+    print("HEllo world")
+
 def greet_all(names: list[str]) -> None:
     for name in names:
         print('Hello ' + name)
